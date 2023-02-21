fc46099e6da59a3bea70d58fa244eeaf67af004a
maheshchavan99 <maheshchavan5226@gmail.com>
2023-02-21 21:29:36 +0530

    master changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { store } from './store/index';
-import { axios } from 'axios';
+// import { axios } from 'axios';
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 // axios.defaults.baseURL = process.env.REACT_APP_BASE_URL
