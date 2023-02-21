ff608a7fe10f39b3b9c5fb5492d9c97fe18f024b
maheshchavan99 <maheshchavan5226@gmail.com>
2023-02-21 20:57:58 +0530

    payment router

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Home from './pages/home/Home'
 
 import { Routes, Route } from "react-router-dom";
 import BookingYoga from './components/bookingclass/BookingYoga';
+import Payment from './components/payment/Payment';
 function App() {
   return (
     <div>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home/>} />
-        <Route path='/booking' element={<BookingYoga/>} />
+        <Route path='/booking' element={<Payment/>} />
       </Routes>
     </div>
   )
